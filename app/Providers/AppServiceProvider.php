95cfde72db906d0b4cb0d917db80641213d55b74
xfathurrahman <fathurrahmanx018@gmail.com>
2023-08-30 05:10:20 +0700

    fix handle https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,13 +21,9 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
+    public function boot(Request $request): void
     {
+        $this->setHttps($request);
         config(['app.locale' => 'id']);
         Carbon::setLocale('id');
         Schema::defaultStringLength(191);
@@ -33,4 +31,16 @@ class AppServiceProvider extends ServiceProvider
             return "Rp. <?php echo number_format($expression, 0, ',', '.'); ?>";
         });
     }
+
+    protected function setHttps(Request $request): void
+    {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        } elseif (app()->environment('local')) {
+            if ((isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] === 'on' || $_SERVER['HTTPS'] === 1)) || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&  $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')) {
+                $request->server->set('HTTPS', true);
+            }
+        }
+    }
+
 }
